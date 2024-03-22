231d5c346908fa85fb70295059ade4976f73f5cb
bimapriaaditya <bpabima@gmail.com>
2024-03-22 22:46:42 +0700

    first test class

---
+++ test/class.test.ts
@@ -0,0 +1,17 @@
+describe('Class Test', () => {
+    it("Should can create a class", () => {
+        
+        class Customer {
+
+        }
+
+        class Order {
+
+        }
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+
+
+    });
+});
